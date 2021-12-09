0729175c94463030cff2eb06fc59bef70d0e1055
datorot93 <herrion1@hotmail.com>
2021-12-08 20:58:27 -0500

    master startin the example of the useRef

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import ReactDOM from 'react-dom';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { CounterApp } from './components/01-useState/CounterApp';
-import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+import { FocusScreen } from './components/04-useRef/FocusScreen';
 
 
 ReactDOM.render(
-    <MultipleCustomHooks />,
+    <FocusScreen />,
     document.getElementById('root')
 );
 
