e8e47249c4bd9e4b4ca575d297480c0349816049
datorot93 <herrion1@hotmail.com>
2021-12-08 20:43:05 -0500

    master simple application to consume an breakingbadapi was added

---
+++ src/hooks/useForm.js
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+// El initialState es el objeto que se declaraba en el useState del useState de SimpleForm.js
+export const useForm = ( initialState = {} ) => {
+
+    const [values, setValues] = useState(initialState)
+
+    const handleInputChange = ({ target }) => {
+
+        setValues({
+            ...values,
+            [ target.name ]: target.value
+        });
+    }
+
+    return [ values, handleInputChange ];
+}
+
